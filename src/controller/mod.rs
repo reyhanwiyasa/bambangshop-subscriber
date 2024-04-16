206827c4e3a506792af5661b06791e2a07bf86a4
reyhanwiyasa <reyhanwiyasa@gmail.com>
2024-04-16 22:28:54 +0700

    Implement list function in Notification controller.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -3,6 +3,6 @@ use rocket::fairing::AdHoc;
 pub fn route_stage() -> AdHoc {
     return AdHoc::on_ignite("Initializing controller routes...", |rocket| async {
         rocket
-            .mount("/", routes![notificiation::subscribe, notificiation::unsubscribe])
+            .mount("/", routes![notificiation::subscribe, notificiation::unsubscribe, notificiation::receive, notificiation::list])
     });
 }
